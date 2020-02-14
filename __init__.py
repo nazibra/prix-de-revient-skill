609247953369fb040888c518e1dbe395d0705314
nazibra <sysinfo@topnegoce.com>
2020-02-14 16:31:38 +0100

    Update __init__.py
    first skill try with articles entities (articles slot equivalent)

--- __init__.py
+++ __init__.py
@@ -5,10 +5,21 @@ from mycroft import MycroftSkill, intent_file_handler
 class PrixDeRevient(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-    @intent_file_handler('revient.de.prix.intent')
-    def handle_revient_de_prix(self, message):
-        self.speak_dialog('revient.de.prix')
+        
+    def initialize(self):
+        self.register_intent_file('prix.de.revient.intent', self.handle_prix_revient)
+    
+    #@intent_file_handler('revient.de.prix.intent')
+    def handle_prix_revient(self, message):
+        article=message.data.get('articles')
+        if article is not None:
+            getdata={'intent':'pr','val':article}
+            resp=requests.get('http://360.topnegoce.com:8000/new/admin/R_Banc_ass/php/SNIPS_ASSET/response.php',params=getdata)
+            rep=resp.text
+            self.speak(rep)
+        else:
+            self.speak('cet article est introuvable.')
+        #self.speak_dialog('revient.de.prix')
 
 
 def create_skill():
