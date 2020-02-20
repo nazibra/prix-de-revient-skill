c6fe7e70878ae767b873871b037e60efeabe5e4e
nazibra <sysinfo@topnegoce.com>
2020-02-20 11:10:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,7 +17,8 @@ class PrixDeRevient(MycroftSkill):
     def handle_stock(self, message):
         article=message.data.get('articles')
         types=message.data.get('typeStock')
-        print(article+'-'+types)
+        print(str(article))
+        print(str(types))
         if article is not None and len(article)>2:
             if types is not None :
                 getdata={'intent':'stock','val':article,'s2':types}
